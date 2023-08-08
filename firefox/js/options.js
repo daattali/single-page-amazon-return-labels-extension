8590240cf8989fe73cd3cfbfce25e96053c6b3e3
Dean Attali <daattali@gmail.com>
2023-08-08 02:08:25 -0400

    add settings popup to firefox; fixes #1

---
+++ firefox/js/options.js
@@ -0,0 +1,26 @@
+var sparlOptions = {
+  init : function() {
+    sparlOptions.restoreOptions();
+    document.getElementById('remove_print').addEventListener('click', function(e) {
+      browser.storage.sync.set({ sparl_remove_print : document.getElementById('remove_print').checked });
+    });
+    document.getElementById('remove_web').addEventListener('click', function(e) {
+      browser.storage.sync.set({ sparl_remove_web : document.getElementById('remove_web').checked });
+    });
+    document.getElementById('remove_items').addEventListener('click', function(e) {
+      browser.storage.sync.set({ sparl_remove_items : document.getElementById('remove_items').checked });
+    });
+  },
+  
+  restoreOptions : function() {
+    browser.storage.sync.get(
+      ['sparl_remove_print', 'sparl_remove_web', 'sparl_remove_items']
+    ).then(function(items) {
+      document.getElementById('remove_print').checked = (typeof items.sparl_remove_print === 'undefined') ? true : items.sparl_remove_print;
+      document.getElementById('remove_web').checked = (typeof items.sparl_remove_web === 'undefined') ? false : items.sparl_remove_web;
+      document.getElementById('remove_items').checked = (typeof items.sparl_remove_items === 'undefined') ? false : items.sparl_remove_items;
+    });
+  }
+}
+
+document.addEventListener('DOMContentLoaded', sparlOptions.init);
